42aa87299d662b6d95946889956859ab093b2754
CapriLucas <capriatalucas@gmail.com>
2021-01-06 19:51:58 -0300

    modifico volumes docker compose

--- services/test/src/routes/index.routes.js
+++ services/test/src/routes/index.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/sahara',(req,res)=>{
-    res.json("ALE PUTO");
+    res.json("Falklands islands");
 });
 module.exports = router;
