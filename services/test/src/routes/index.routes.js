07f0032dc044a4dd6b3813244635a357ee86e78f
CapriLucas <capriatalucas@gmail.com>
2021-01-07 10:52:31 -0300

    fixed json error

--- services/test/src/routes/index.routes.js
+++ services/test/src/routes/index.routes.js
@@ -6,6 +6,6 @@ router.get('/sahara', async(req,res)=>{
     await pool.query("INSERT INTO message (text) VALUES ($1)",['hola']);
     const response = await pool.query("SELECT * FROM message");
     console.log(response.rows);
-    res.json("Falklands islands");
+    res.json(response.rows[0].text);
 });
 module.exports = router;
